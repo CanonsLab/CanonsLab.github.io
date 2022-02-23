3c258981bf9acdfa883eff171c2a2eafdc6b6aef
CanonsLab <canonjensen@gmail.com>
2022-02-23 16:38:20 -0700

    updates to all work

--- my-script.js
+++ my-script.js
@@ -25,17 +25,7 @@ const d = new Date();
 const dayName = daynames[d.getDay()];
 const monthName = months[d.getMonth()];
 const year = d.getFullYear();
-//const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
-const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
-// using getElementById
-document.getElementById("currentdate").textContent = fulldate;
-// ****************************
-// using querySelector
-const date2 = document.querySelector('#currentdate2');
 
-try {
-  const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-  date2.textContent = new Date().toLocaleDateString('en-UK', options);
-} catch (e) {
-  alert('Error with code or your browser does not support Locale');
-}
+const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+
+document.querySelector('#currentdate2').textContent = `Last updated: ${document.lastModified}`
