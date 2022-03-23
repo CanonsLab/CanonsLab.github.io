0322039610c1fea2194edc5cd7821f14f83df557
CanonsLab <canonjensen@gmail.com>
2022-03-23 13:34:09 -0600

    added content for lesson 5

---
+++ lesson-5/home-script.js
@@ -0,0 +1,20 @@
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject);
+        const towns = jsonObject['towns'];
+        for (let i = 0; i < towns.length; i++ ) {
+
+        }
+    })
