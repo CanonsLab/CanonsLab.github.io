8606409ce575f026de421cf4a69c2d0cd51fda6b
CanonsLab <canonjensen@gmail.com>
2022-03-23 15:51:26 -0600

    finished weather home page

--- lesson-5/home-script.js
+++ lesson-5/home-script.js
@@ -14,7 +14,28 @@ fetch(requestURL)
     .then(function (jsonObject) {
         console.table(jsonObject);
         const towns = jsonObject['towns'];
-        for (let i = 0; i < towns.length; i++ ) {
+    for (let i = 0; i < towns.length; i++ ) {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        h2.textContent = towns[i].name;
+        card.appendChild(h2)
+        let h4 = document.createElement('h4');
+        h4.textContent = towns[i].motto;
+        card.appendChild(h4);
+        let founded = document.createElement('p');
+        founded.textContent = 'Year Founded: ' + towns[i].yearFounded;
+        card.appendChild(founded);
+        let population = document.createElement('p');
+        population.textContent = 'Population: ' + towns[i].currentPopulation;
+        card.appendChild(population);
+        let rain = document.createElement('p');
+        rain.textContent = 'Annual Rain Fall: ' + towns[i].averageRainFall;
+        card.appendChild(rain);
+        let image = document.createElement('img');
+        image.setAttribute('src', towns[i].photo);
+        image.setAttribute('alt', towns[i].name + ' photo');
+        card.appendChild(image);
 
-        }
-    })
+        document.querySelector('div.container').appendChild(card);
+    }
+    });
