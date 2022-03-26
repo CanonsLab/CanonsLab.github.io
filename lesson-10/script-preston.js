502091fd5b90d5e8fed39b95bbb480fb72593468
CanonsLab <canonjensen@gmail.com>
2022-03-26 12:08:17 -0600

    5-day forecast API

--- lesson-10/script-preston.js
+++ lesson-10/script-preston.js
@@ -1,15 +1,47 @@
-const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=3c3f395856a764140e870d104facaf6d'
+const weatherInfo = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=3c3f395856a764140e870d104facaf6d'
+const forecastInfo = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=3c3f395856a764140e870d104facaf6d'
 
-fetch(apiURL)
+
+fetch(weatherInfo)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
     document.getElementById('current-temp').textContent = jsObject.main.temp;
-	document.getElementById('weather-description').textContent = jsObject.main.description;
+	document.getElementById('weather-description').textContent = jsObject.weather[0].description;
     document.getElementById('high-temp').textContent = jsObject.main.temp_max;
 	document.getElementById('humidity').textContent = jsObject.main.humidity;
 	document.getElementById('wind-speed').textContent = jsObject.wind.speed;
   });
+fetch(forecastInfo)
+	.then((response) => response.json())
+	.then((jsObject) => {
+		console.log(jsObject);
+		
+		const datesOfWeather = jsObject["list"].filter((climate) =>{
+			if (climate.dt_txt.includes(" 18:00:00")){
+				return climate
+			}})
+			for (let x = 0; x < datesOfWeather.length; x++) {
+				let card = document.createElement("section")
+				card.setAttribute("class", "day-weather")
+				let h3d = document.createElement("span")
+				h3d.textContent = daynames[new Date(datesOfWeather[x].dt_txt).getDay()]
+				h3d.setAttribute("class", "dayname")
+				card.appendChild(h3d)
+				document.querySelector("div.flex").appendChild(card)
+				let img = document.createElement("img")
+				img.setAttribute("src", 'https://openweathermap.org/img/w/' + datesOfWeather[x].weather[0].icon + '.png')
+				img.setAttribute("alt", datesOfWeather[x].weather[0].description)
+				img.setAttribute("id", "dayimg")
+				card.appendChild(img)
+				let h4t = document.createElement("span")
+				h4t.textContent = datesOfWeather[x].main.temp + "°F"
+				h4t.setAttribute("class", "day-label")
+				card.appendChild(h4t)
+
+			}
+	}
+	)
 
 
 const hambutton = document.querySelector('.ham');
