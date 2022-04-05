f321bdbe65d729584ef7207d0b53a0f79e49a4cb
CanonsLab <canonjensen@gmail.com>
2022-04-04 21:58:04 -0600

    script, style and date modified

--- wdd-230-final-project/js/script.js
+++ wdd-230-final-project/js/script.js
@@ -12,6 +12,29 @@ const daynames = [
 	"Saturday"
 ];
 
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+
+const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+
+document.querySelector('#currentdate2').textContent = `Last updated: ${document.lastModified}`
 
 fetch(weatherInfo)
   .then((response) => response.json())
